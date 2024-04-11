4fb941ca7cd2e0d6dfec555fc1232f75f233f0e6
Anoop Hallur <anoophallur.com>
2024-04-11 09:12:06 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-unstable branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.hpspectre = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+  	# make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # TODO replace ryan with your own username
+          home-manager.users.anoop = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
+    };
+  };
+}
