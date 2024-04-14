ad06a9639e028ea5c6d9daf1c7fe45a857221c3d
Anoop Hallur <anoophallur.com>
2024-04-14 15:43:10 +0530

    Fix xfce final

--- home-manager/xfce-home.nix
+++ home-manager/xfce-home.nix
@@ -29,148 +29,137 @@
 
   xfconf.settings = {
     xfce4-keyboard-shortcuts = {
-      "/commands/custom/override" = true; # allow custom commands
-      "/commands/custom/<Super>c" = "xfce4-popup-clipman";    
-      "/commands/custom/<Alt>F2" = "xfce4-popup-whiskermenu";
+      "commands/custom/override" = true; # allow custom commands
+      "commands/custom/<Super>c" = "xfce4-popup-clipman";    
+      "commands/custom/<Alt>F2" = "xfce4-popup-whiskermenu";
 
       # allow custom wm keybindings
-      "/xfwm4/custom/override" = true;
-      "/xfwm4/custom/<Alt><Shift>Tab" = "cycle_reverse_windows_key";
-      "/xfwm4/custom/<Alt>Tab" = "cycle_windows_key";
-      "/xfwm4/custom/<Super>Tab" = "switch_window_key";
-      "/xfwm4/custom/<Super>Up" = "maximize_window_key";
-      "/xfwm4/custom/<Super>Down" = "hide_window_key";
+      "xfwm4/custom/override" = true;
+      "xfwm4/custom/<Alt><Shift>Tab" = "cycle_reverse_windows_key";
+      "xfwm4/custom/<Alt>Tab" = "cycle_windows_key";
+      "xfwm4/custom/<Super>Tab" = "switch_window_key";
+      "xfwm4/custom/<Super>Up" = "maximize_window_key";
+      "xfwm4/custom/<Super>Down" = "hide_window_key";
 
       # windows tiling
-      "/xfwm4/custom/<Super>Left" = "tile_left_key";
-      "/xfwm4/custom/<Super>Right" = "tile_right_key";
+      "xfwm4/custom/<Super>Left" = "tile_left_key";
+      "xfwm4/custom/<Super>Right" = "tile_right_key";
 
       # Moving across monitors
-      "/xfwm4/custom/<Shift><Super>Left" = "move_window_to_monitor_left_key";
-      "/xfwm4/custom/<Shift><Super>Right" = "move_window_to_monitor_right_key";
-      "/xfwm4/custom/<Shift><Super>Up" = "move_window_to_monitor_up_key";
-      "/xfwm4/custom/<Shift><Super>Down" = "move_window_to_monitor_down_key";
+      "xfwm4/custom/<Shift><Super>Left" = "move_window_to_monitor_left_key";
+      "xfwm4/custom/<Shift><Super>Right" = "move_window_to_monitor_right_key";
+      "xfwm4/custom/<Shift><Super>Up" = "move_window_to_monitor_up_key";
+      "xfwm4/custom/<Shift><Super>Down" = "move_window_to_monitor_down_key";
     };
     xfce4-session = {
-      "/startup/ssh-agent/enabled" = false;
-      "/general/LockCommand" = "${pkgs.lightdm}/bin/dm-tool lock";
+      "startup/ssh-agent/enabled" = false;
+      "general/LockCommand" = "${pkgs.lightdm}/bin/dm-tool lock";
     }; 
     xfwm4 = {
-      "/general/workspace_count" = 4;
-      "/general/workspace_names" = [ "1" "2" "3" "4"];
-      "/general/theme" = "Default";
-     #"/general/easy_click" = "Super"; # key used to grab and move windows
-      "/general/snap_to_windows" = true;
-      "/general/activate_action" = "switch";
-      "/general/wrap_cycle" = true; # cycle to first workspace after last
-      "/general/cycle_draw_frame" = false; # no blue frame when cycling with alt-tab
-      "/general/cycle_preview" = true; # thumbnails when cycling with alt-tab
-      "/general/cycle_tabwin_mode" = 0; # don’t cycle through windows in a list
-      "/general/wrap_windows" = false; # don’t move window to other workspace when approaching border
+      "general/workspace_count" = 4;
+      "general/workspace_names" = [ "1" "2" "3" "4"];
+      "general/theme" = "Default";
+     #"general/easy_click" = "Super"; # key used to grab and move windows
+      "general/snap_to_windows" = true;
+      "general/activate_action" = "switch";
+      "general/wrap_cycle" = true; # cycle to first workspace after last
+      "general/cycle_draw_frame" = false; # no blue frame when cycling with alt-tab
+      "general/cycle_preview" = true; # thumbnails when cycling with alt-tab
+      "general/cycle_tabwin_mode" = 0; # don’t cycle through windows in a list
+      "general/wrap_windows" = false; # don’t move window to other workspace when approaching border
     };
     xfce4-panel = {      
-      "/panels" = [ 1 ];
-      "/panels/dark-mode" = true;
-      "/panels/panel-1/nrows" = 1; # number of rows
-      "/panels/panel-1/mode" = 0; # Horizontal
-      "/panels/panel-1/icon-size" = 0; # Adjust size automatically
-      "/panels/panel-1/size" = 26; # Row size (pixels)
-      "/panels/panel-1/length" = 100.0;
-      "/panels/panel-1/position" = "p=8;x=960;y=1066";
-      "/panels/panel-1/position-locked" = true;
-      "/panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ];
+      "panels" = [ 1 ];
+      "panels/dark-mode" = true;
+      "panels/panel-1/nrows" = 1; # number of rows
+      "panels/panel-1/mode" = 0; # Horizontal
+      "panels/panel-1/icon-size" = 0; # Adjust size automatically
+      "panels/panel-1/size" = 26; # Row size (pixels)
+      "panels/panel-1/length" = 100.0;
+      "panels/panel-1/position" = "p=8;x=960;y=1066";
+      "panels/panel-1/position-locked" = true;
+      "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ];
       
       # Launcher = whiskermenu
-      "/plugins/plugin-1" = "whiskermenu";
-      "/plugins/plugin-1/view-mode" = 0;
+      "plugins/plugin-1" = "whiskermenu";
+      "plugins/plugin-1/view-mode" = 0;
   
       # Application Menu
-      "/plugins/plugin-2" = "applicationsmenu";
-      "/plugins/plugin-2/show-button-title" = false;
+      "plugins/plugin-2" = "applicationsmenu";
+      "plugins/plugin-2/show-button-title" = false;
   
       # Tasklist
-      "/plugins/plugin-3" = "tasklist";
-      "/plugins/plugin-3/grouping" = false;
-      "/plugins/plugin-3/sort-order" = 01; # Group title and timestamp
+      "plugins/plugin-3" = "tasklist";
+      "plugins/plugin-3/grouping" = false;
+      "plugins/plugin-3/sort-order" = 01; # Group title and timestamp
   
       # Separator
-      "/plugins/plugin-4" = "separator";
-      "/plugins/plugin-4/style" = 0; # transparent
-      "/plugins/plugin-4/expand" = true;
+      "plugins/plugin-4" = "separator";
+      "plugins/plugin-4/style" = 0; # transparent
+      "plugins/plugin-4/expand" = true;
   
       # Workspaces
-      "/plugins/plugin-5" = "pager";
-      "/plugins/plugin-5/rows" = 1;
-      "/plugins/plugin-5/wrap-workspaces" = true;
-      "/plugins/plugin-5/numbering" = true;
-      "/plugins/plugin-5/workspace-scrolling" = false;
+      "plugins/plugin-5" = "pager";
+      "plugins/plugin-5/rows" = 1;
+      "plugins/plugin-5/wrap-workspaces" = true;
+      "plugins/plugin-5/numbering" = true;
+      "plugins/plugin-5/workspace-scrolling" = false;
   
       # Separator
-      "/plugins/plugin-6" = "separator";
-      "/plugins/plugin-6/style" = 0; # transparent
+      "plugins/plugin-6" = "separator";
+      "plugins/plugin-6/style" = 0; # transparent
   
       # Sys tray
-      "/plugins/plugin-7" = "systray";
-      "/plugins/plugin-7/icon-size" = 0;
+      "plugins/plugin-7" = "systray";
+      "plugins/plugin-7/icon-size" = 0;
   
       # clipboard
-      "/plugins/plugin-8" = "xfce4-clipman-plugin";
-      "/plugins/plugin-8/settings/save-on-quit" = true;
-      "/plugins/plugin-8/settings/max-texts-in-history" = 1000;
-      "/plugins/plugin-8/settings/add-primary-clipboard" = false;
+      "plugins/plugin-8" = "xfce4-clipman-plugin";
+      "plugins/plugin-8/settings/save-on-quit" = true;
+      "plugins/plugin-8/settings/max-texts-in-history" = 1000;
+      "plugins/plugin-8/settings/add-primary-clipboard" = false;
   
       # Pulse audio
-      "/plugins/plugin-9" = "pulseaudio";
-      "/plugins/plugin-9/enable-keyboard-shortcuts" = true;
-      "/plugins/plugin-9/multimedia-keys-to-all" = true;
-      "/plugins/plugin-9/play-sound" = true;
-      "/plugins/plugin-9/show-notifications" = true;
+      "plugins/plugin-9" = "pulseaudio";
+      "plugins/plugin-9/enable-keyboard-shortcuts" = true;
+      "plugins/plugin-9/multimedia-keys-to-all" = true;
+      "plugins/plugin-9/play-sound" = true;
+      "plugins/plugin-9/show-notifications" = true;
   
       # Power manager
-      "/plugins/plugin-10" = "power-manager-plugin";
+      "plugins/plugin-10" = "power-manager-plugin";
   
       # Notification
-      "/plugins/plugin-11" = "notification-plugin";
+      "plugins/plugin-11" = "notification-plugin";
   
       # Separator
-      "/plugins/plugin-12" = "separator";
-      "/plugins/plugin-12/style" = 1; # non-transparent
+      "plugins/plugin-12" = "separator";
+      "plugins/plugin-12/style" = 1; # non-transparent
   
       # Clock
-      "/plugins/plugin-13" = "clock";
-      "/plugins/plugin-13/digital-layout" = 3; # Time Only
-      "/plugins/plugin-13/digital-time-format" = "%A, %B %d, %Y   %H:%m:%S %P"; # Saturday, April 13, 2024   10:04:34
-      "/plugins/plugin-13/tooltip-format" = "%A %d %B %Y"; # Saturday 29 July 2023
-      "/plugins/plugin-13/mode" = 2; # digital
-      "/plugins/plugin-13/show-frame" = false;
+      "plugins/plugin-13" = "clock";
+      "plugins/plugin-13/digital-layout" = 3; # Time Only
+      "plugins/plugin-13/digital-time-format" = "%A, %B %d, %Y   %H:%m:%S %P"; # Saturday, April 13, 2024   10:04:34
+      "plugins/plugin-13/tooltip-format" = "%A %d %B %Y"; # Saturday 29 July 2023
+      "plugins/plugin-13/mode" = 2; # digital
+      "plugins/plugin-13/show-frame" = false;
   
       # Separator
-      "/plugins/plugin-14" = "separator";
-      "/plugins/plugin-14/style" = 1; # non-transparent
+      "plugins/plugin-14" = "separator";
+      "plugins/plugin-14/style" = 1; # non-transparent
   
       # Show Dekstop 
-      "/plugins/plugin-15" = "showdesktop";  
+      "plugins/plugin-15" = "showdesktop";  
     };
     xfce4-appfinder = {
-      "/hide-window-decorations" = false;
-      "/always-center" = true;
-      "/sort-by-frecency" = true;
-      "/remember-category" = true;
+      "hide-window-decorations" = false;
+      "always-center" = true;
+      "sort-by-frecency" = true;
+      "remember-category" = true;
     };
     xsettings = {
-      "/Net/ThemeName" = "Adwaita";
-      "/Net/IconThemeName" = "Adwaita";
+      "Net/ThemeName" = "Adwaita";
+      "Net/IconThemeName" = "Adwaita";
     };
   };
-
-  services.xcape = {
-    enable = true;
-    timeout = 500; # ms
-    mapExpression = {
-      # Map Windows (or Super) Key so that behaves as Alt+F2 when pressed and released
-      # within the timeout. For Invoking whisker menu popup
-      Super_L = "Alt_L|F2";
-    };
-  };
-
 }
