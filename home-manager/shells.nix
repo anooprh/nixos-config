84182664ff477d8f71e39538077a904d2305aaca
Anoop Hallur <anoophallur.com>
2024-04-22 22:05:16 +0530

    cleanup

--- home-manager/shells.nix
+++ home-manager/shells.nix
@@ -19,32 +19,4 @@
       theme = "robbyrussell";
     };
   };
-
-  #programs.bash = {
-  #  enable = true;
-  #  enableCompletion = true;
-  #  # TODO add your custom bashrc here
-  #  bashrcExtra = ''
-  #    export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-  #  '';
-
-  #  # set some aliases, feel free to add more or remove some
-  #  shellAliases = {
-  #    k = "kubectl";
-  #    urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-  #    urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-  #  };
-  #};
-
-  ## starship - an customizable prompt for any shell
-  #programs.starship = {
-  #  enable = true;
-  #  # custom settings
-  #  settings = {
-  #    add_newline = false;
-  #    aws.disabled = true;
-  #    gcloud.disabled = true;
-  #    line_break.disabled = true;
-  #  };
-  #};
 }
