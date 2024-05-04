cfd1a5f1b2795716b17abab72020d3e88144cb22
Anoop Hallur <anoophallur.com>
2024-05-05 02:16:00 +0530

    add vala plugin

--- xfce.nix
+++ xfce.nix
@@ -36,10 +36,13 @@
       xfce.xfce4-dict
       xfce.xfce4-fsguard-plugin
       xfce.xfce4-genmon-plugin
+      xfce.xfce4-mpc-plugin
       xfce.xfce4-netload-plugin
       xfce.xfce4-panel
+      xfce.xfce4-power-manager
       xfce.xfce4-pulseaudio-plugin
       xfce.xfce4-systemload-plugin
+      xfce.xfce4-timer-plugin
       xfce.xfce4-volumed-pulse
       xfce.xfce4-weather-plugin
       xfce.xfce4-whiskermenu-plugin
