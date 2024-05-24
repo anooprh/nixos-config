3ad19f080c2203dbe9dd1a84b009fc7849e97393
Anoop Hallur <anoophallur@gmail.com>
2024-05-24 19:03:40 +0530

    Customize gnome

--- home/anoop/desktopEnvironments/gnome/default.nix
+++ home/anoop/desktopEnvironments/gnome/default.nix
@@ -1,5 +1,62 @@
 { inputs, config, pkgs, lib, ... }:
 
 {
+  
+  gtk = {
+    enable = true;
 
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  dconf.settings = {
+   "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      workspace-names = [ "Main" ];
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      # `gnome-extensions list` for a list
+      enabled-extensions = [
+        "Vitals@CoreCoding.com"
+        "sound-output-device-chooser@kgshank.net"
+      ];
+    };
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "palenight";
+    };
+  };
+
+  home.packages = with pkgs; [
+    gnomeExtensions.vitals
+    gnomeExtensions.sound-output-device-chooser
+    palenight-theme
+  ];
 }
