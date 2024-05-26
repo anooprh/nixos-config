b84524e8e856d3a8ed1ea6b2d3fd33433b9f35be
Anoop Hallur <anoophallur@gmail.com>
2024-05-26 17:11:36 +0530

    add stylix wallpaper

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -130,4 +130,6 @@
   virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.seamless = true;
+
+  stylix.image = ./../wallpapers/sample.jpg;
 }
