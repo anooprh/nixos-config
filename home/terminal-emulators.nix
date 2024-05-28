090339f464f94c0e313e8f2f2fd8784da36c8dfc
Anoop Hallur <anoophallur@gmail.com>
2024-05-28 17:19:09 +0530

    docker, vscode

--- home/terminal-emulators.nix
+++ home/terminal-emulators.nix
@@ -4,8 +4,19 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      config.enable_scroll_bar = true
-      scrollback_lines = 20000;
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.enable_scroll_bar = true
+config.scrollback_lines = 20000;
+
+-- and finally, return the configuration to wezterm
+return config
+
     '';
   };
 }
