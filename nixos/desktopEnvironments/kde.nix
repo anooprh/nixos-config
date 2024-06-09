cdd09ce444654ea6ce12a5056eda75e37f280b9d
Anoop Hallur <anoophallur@gmail.com>
2024-06-09 21:55:51 +0530

    Rename sddm and plasma variables

--- nixos/desktopEnvironments/kde.nix
+++ nixos/desktopEnvironments/kde.nix
@@ -2,8 +2,8 @@
 
 {
   # Enable KDE Plasma 6
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   environment.systemPackages = with pkgs; [
