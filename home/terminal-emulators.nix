92ad9f96545d2760d9ca97a963b242b939e65a5e
Anoop Hallur <anoophallur@gmail.com>
2024-06-09 23:48:30 +0530

    configure terminator

--- home/terminal-emulators.nix
+++ home/terminal-emulators.nix
@@ -1,5 +1,21 @@
 { inputs, config, pkgs, lib, ... }:
 
 {
-  programs.terminator.enable = true;
+
+  home.packages = with pkgs; [
+    jetbrains-mono
+  ];
+
+  programs.terminator = { 
+    enable = true;
+    config = {
+      profiles.default.use_system_font = false;
+      profiles.default.scrollback_infinite = true;
+      profiles.default.audible_bell = true;
+      profiles.default.visible_bell = true;
+      profiles.default.urgent_bell = true;
+      profiles.default.cursor_shape = "ibeam";
+      profiles.default.font = "JetBrains Mono 11";
+    };
+  };
 }
