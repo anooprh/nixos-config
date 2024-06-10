f28619576388cb3ec02c3e6e7e9eab05e16e6f15
Anoop Hallur <anoophallur@gmail.com>
2024-06-10 18:28:13 +0530

    Add kde specifics

--- home/kde.nix
+++ home/kde.nix
@@ -6,50 +6,71 @@
     enable = true;
     overrideConfig = true;
 
-   panels = [
-     {
-      location = "top";
-      height = 30;
-      widgets = [
-        "org.kde.plasma.kickoff"
-        {
-          name = "org.kde.plasma.icontasks";
-          config = {
-            General.launchers = [
-              "applications:org.kde.dolphin.desktop"
-              "applications:org.kde.konsole.desktop"
-            ];
-          };
-        }
-        "org.kde.plasma.marginsseparator"
-        "org.kde.plasma.appmenu"
-        "org.kde.plasma.panelspacer"
-
-        {
-          name = "org.kde.plasma.weather";
-          config = {
-            WeatherStation.source = "bbcukmet|weather|Hyderabad, India, IN|1269843";
-            Appearance.showTemperatureInCompactMode = "true";
-          };
-        }
-        "org.kde.plasma.marginsseparator"
-        {
-          name = "org.kde.plasma.systemtray";  
-        }
-        {
-          digitalClock = {
-            calendar.firstDayOfWeek = "monday";
-            time.format = "12h";
-            time.showSeconds = "always";
-            date.position = "besideTime";
-            date.format = {
-              "custom" = "dddd dd MMMM yyyy";
+    panels = [
+      {
+        location = "top";
+        height = 36;
+        widgets = [
+          "org.kde.plasma.kickoff"
+          {
+            name = "org.kde.plasma.icontasks";
+            config = {
+              General.launchers = [
+                "applications:org.kde.dolphin.desktop"
+                "applications:org.kde.konsole.desktop"
+              ];
             };
-          };
-        }
-        "org.kde.plasma.showdesktop"
-      ];
-     }
-   ];
+          }
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.appmenu"
+          "org.kde.plasma.panelspacer"
+
+          {
+            name = "org.kde.plasma.weather";
+            config = {
+              WeatherStation.source = "bbcukmet|weather|Hyderabad, India, IN|1269843";
+              Appearance.showTemperatureInCompactMode = "true";
+            };
+          }
+          "org.kde.plasma.marginsseparator"
+          {
+            systemTray.items = {
+              # Explicitly show these items
+              shown = [
+                "org.kde.plasma.battery"
+                "org.kde.plasma.bluetooth"
+                "org.kde.plasma.volume"
+                "org.kde.plasma.networkmanagement"
+                "org.kde.plasma.bluetooth"
+                "org.kde.plasma.clipboard"
+                "org.kde.plasma.mediacontroller"
+                "org.kde.kscreen"
+              ];
+              # Explicitly hide these items
+              hidden = [
+              ];
+            };
+          }
+          {
+            digitalClock = {
+              calendar.firstDayOfWeek = "monday";
+              time.format = "12h";
+              time.showSeconds = "always";
+              date.position = "adaptive";
+              date.format = {
+                "custom" = "dddd dd MMMM yyyy";
+              };
+            };
+          }
+          "org.kde.plasma.showdesktop"
+        ];
+      }
+    ];
+    shortcuts = {
+      services = {
+        "org.flameshot.Flameshot.deskto" = [ "Capture" "Print" ];
+      };
+    };
+
   };
 }
